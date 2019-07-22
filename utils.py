4ad9eadfdb066283095d9a2323e5ea08cf101803
helloworldexpert <35606941+helloworldexpert@users.noreply.github.com>
2019-07-21 22:33:57 -0500

    added pca plot

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ import pandas as pd
 #
 # Inputs: df  (training data. each column is a numerical feature)
 
-def feature_normalization(df):
+def standardize_features(df):
     return (df - df.mean()) / df.std()
 
 
@@ -20,44 +20,54 @@ def feature_normalization(df):
 
 from sklearn.decomposition import PCA
 
-# Performs feature selection on data in the form of a dataframe.
+# Performs principal component analysis on data df (type dataframe).
 # The data is transformed so that the resulting features are 
-# principal components are explain a specified percentage of total variance.
+# principal components that explain a specified percentage of total variance.
 #
 # Inputs: df           (training data as a dataframe. each column is a feature.)
 #         cutoff_var   (percentage of total variance to explain. must satisfy
 #                       0 < cutoff < 1.)
 #         cutoff_feat  (maximum number of principal components to use. must
 #                       satisfy 0 < cutoff_feat < number of columns in df)
-#         standardized (set to true if each feature in df has 0 mean and unit
-#                       variance)
+#         standardize  (set to true to standardize columns so that each has
+#                       0 mean and unit variance)
 
-def feature_selection_pca(df, co_var=0.9, co_feat=None, standardized=False):
+def pca(df, co_var=0.9, co_feat=None, standardize=True):
     
-    if standardized == False: df = feature_normalization(df)
+    if standardize == True: df = standardize_features(df)
     
     if co_feat == None: co_feat = df.shape[1]
-    pca = PCA(n_components=co_feat)
-    pca.fit(df)
+    pca_0 = PCA(n_components=co_feat)
+    pca_0.fit(df)
     
     # find the number of principal components needed to explain
     # the specified cutoff variance
     n_component = 0
     var_sum = 0
-    for var in pca.explained_variance_ratio_:
+    for var in pca_0.explained_variance_ratio_:
         n_component += 1
         var_sum += var
-        if var_sum > co_var or n_component >= co_feat: break
+        if var_sum > min(co_var, 1.0) or n_component >= co_feat: break
         
-    # create dataframe with principal components as features   
+    # create dataframe with new features that are linear combinations of the
+    # old features and use principal components as weight vectors
     df_new = pd.DataFrame()    
     for pc_num in range(n_component):
         df_new[f'pc_{pc_num}'] = np.zeros(df.shape[0])
         for weight_num in range(df.shape[1]):
-            df_new[f'pc_{pc_num}'] += pca.components_[pc_num, weight_num] * df.iloc[:, weight_num]
+            df_new[f'pc_{pc_num}'] += pca_0.components_[pc_num, weight_num] * df.iloc[:, weight_num]
         
     print(f'Number of principal components: {n_component}')
     print(f'Percent of total variance explained: {var_sum}')
+    
+    x = range(n_component)
+    y = pca_0.explained_variance_ratio_[0:n_component]
+    plt.bar(x, y)
+    plt.title('% of total variance explained by each principal component returned')
+    plt.xlabel('Principal components')
+    plt.ylabel('% of total variance explained')
+    plt.show()
+    
     return df_new
 
 
